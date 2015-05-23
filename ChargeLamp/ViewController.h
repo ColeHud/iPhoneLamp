37d9a002ae325a4ddd75d83e54c1c5999f87dead
ColeHud <cmhudson11@gmail.com>
2015-05-23 16:01:41 -0400

    Initial Commit

---
+++ ChargeLamp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChargeLamp
+//
+//  Created by Cole on 5/23/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
